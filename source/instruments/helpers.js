f518ddffba7c3da4668ec6fad8271d4eca9d896d
Serhii Tanchenko <serg.tanchenko@gmail.com>
2019-03-20 23:45:27 +0200

    sort tasks by creation date

--- source/instruments/helpers.js
+++ source/instruments/helpers.js
@@ -2,7 +2,7 @@ export function getDisplayName (WrappedComponent) {
     return WrappedComponent.displayName || WrappedComponent.name || "Component";
 }
 
-const compareObjectsByProperty = (a, b, propertyName) => {
+const booleanPropertyComparator = (a, b, propertyName) => {
     if (a.get(propertyName) && !b.get(propertyName)) {
         return -1;
     } else if (!a.get(propertyName) && b.get(propertyName)) {
@@ -12,18 +12,39 @@ const compareObjectsByProperty = (a, b, propertyName) => {
     return 0;
 };
 
-export const sortTasks = (tasks) => {
-    const favoriteUnaccomplished = tasks.filter(
-        (task) => task.get("favorite") && !task.get("completed")
-    );
-    const unaccomplished = tasks.filter(
-        (task) => !task.get("favorite") && !task.get("completed")
-    );
+const datePropertyComparator = (a, b, propertyName) => {
+    let date1 = a.get(propertyName);
+    let date2 = b.get(propertyName);
 
-    const completed = tasks.filter((task) => task.get("completed"));
+    if (!(date1 instanceof Date)) {
+        date1 = new Date(date1);
+    }
+
+    if (!(date2 instanceof Date)) {
+        date2 = new Date(date2);
+    }
+
+    return date1.getTime() - date2.getTime();
+};
+
+export const sortTasks = (tasks) => {
+    const sortByCreationDate = (t1, t2) =>
+        datePropertyComparator(t1, t2, "created");
+
+    const favoriteUnaccomplished = tasks
+        .filter((task) => task.get("favorite") && !task.get("completed"))
+        .sort(sortByCreationDate);
+
+    const unaccomplished = tasks
+        .filter((task) => !task.get("favorite") && !task.get("completed"))
+        .sort(sortByCreationDate);
+
+    const completed = tasks
+        .filter((task) => task.get("completed"))
+        .sort(sortByCreationDate);
 
     const sortedCompleted = completed.sort((t1, t2) =>
-        compareObjectsByProperty(t1, t2, "favorite")
+        booleanPropertyComparator(t1, t2, "favorite")
     );
 
     return [...favoriteUnaccomplished, ...unaccomplished, ...sortedCompleted];
